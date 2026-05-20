389e20f324d6ec2596e6ee9c9f88a1db490672a3
camiloperez123 <capo13072002@gmail.com>
2026-05-19 23:18:50 -0400

    feat: configuracion base del proyecto

---
+++ server/utils/prisma.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '../../generated/prisma/client'
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+const globalForPrisma = globalThis as unknown as {
+  __prisma?: PrismaClient 
+}
+
+
+function createPrismaClient() {
+  const url = new URL(process.env.DATABASE_URL!)
+  const adapter = new PrismaMariaDb({
+    host: url.hostname,
+    port: Number(url.port) || 3306,
+    user: decodeURIComponent(url.username),
+    database: url.pathname.slice(1),
+    connectionLimit: 5,
+  })
+  return new PrismaClient({ adapter , log: ['error', 'warn'] })
+}
+
+
+export const prisma: PrismaClient =
+  globalForPrisma.__prisma ?? createPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.__prisma == prisma
+}
